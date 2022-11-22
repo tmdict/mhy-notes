f27a6f9e351c7015c8fb9e5f77097d982c6f982e
Shawn Doria <gumugum@gmail.com>
2022-11-21 23:56:16 -0800

    clean up

--- src/lib/components/content/ScalingFaq/En.svelte
+++ src/lib/components/content/ScalingFaq/En.svelte
@@ -1,1 +1,13 @@
-<h5>关于</h5>
+<h5>About</h5>
+
+<p>
+  Information on which stats each character scales with. Can be useful when deciding whether an artifact with certain
+  main and sub stats is good for a certain character.
+</p>
+
+<p>
+  Most of the information is derived from the <a href="https://genshin-impact.fandom.com/wiki/Character/Comparison"
+    >Genshin Impact Wiki</a
+  >. Crit stats, as well as stats for a character's most recommended talent (based on their optimal/meta build) is
+  highlighted, as these tend to be the most desirable stats when choosing artifacts and weapons.
+</p>
